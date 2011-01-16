23852540bb62e43d492bc6a63e4f96d4eaf5fb6b
albertwcheng <albertwcheng@gmail.com>
2011-01-16 03:56:45 -0500

    commit

--- make.sh
+++ make.sh
@@ -1,1 +1,20 @@
+
+
+if [ ! -e samtools ]; then
+  echo "samtools not exist. downlaod samtools and put inside this folder. abort"
+  exit
+fi
+
+if [ ! -e samtools/libbam.a ]; then
+	echo "samtools not built. Building will proceed now"
+	cd samtools
+	make
+	cd ..
+fi
+
+if [ ! -e samtools/libbam.a ]; then 
+	echo "samtools build failed. abort"
+	exit
+fi
+
 g++ -Wall -o clustergram -I./samtools/ -L./samtools/ -lbam -lm -lz *.cpp samtools/libbam.a
